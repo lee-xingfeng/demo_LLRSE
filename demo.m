7ac3b26858e251744bf352e0512aef379a021df8
ZHENWEN REN <49824429+renzhenwen@users.noreply.github.com>
2019-07-21 09:49:44 +0800

    Update demo.m

--- demo.m
+++ demo.m
@@ -13,9 +13,9 @@ lambda3 = 0.0001;%Z
 [P,Q,Z,E,H,obj] = LLRSE(Train_Ma,Train_Lab,lambda1,lambda2,lambda3);
 
 Test_Maa  = Q'*Test_Ma;
-Test_Maa  = Test_Maa./repmat(sqrt(sum(Test_Maa.^2)),[size(Test_Maa,1) 1]);
+Test_Maa = NormalizeFea(Test_Maa,0);
 Train_Maa = Q'*Train_Ma;
-Train_Maa = Train_Maa./repmat(sqrt(sum(Train_Maa.^2)),[size(Train_Maa,1) 1]);
+Train_Maa = NormalizeFea(Train_Maa,0);
 [pred] = knnclassify(real(Test_Maa'),real(Train_Maa'),Train_Lab,1,'euclidean','nearest');
 acc_test     = sum(Test_Lab == pred)/length(Test_Lab)*100
 
