c1d24a067cbe2e4a57ad2dfb0a2404816fed4787
ZHENWEN REN <49824429+renzhenwen@users.noreply.github.com>
2019-07-21 09:50:52 +0800

    Update demo.m

--- demo.m
+++ demo.m
@@ -4,18 +4,18 @@ clear memory;
 %addpath('./lib');
 
 
-[Train_Ma,Train_Lab,Test_Ma,Test_Lab]=gen_data_random('YaleB_32x32',10);
+[TrainData,TrainLab,TestData,TestLab]=gen_data_random('YaleB_32x32',10);
 
 lambda1 = 0.1;%Q
 lambda2 = 0.001;%E
 lambda3 = 0.0001;%Z
 
-[P,Q,Z,E,H,obj] = LLRSE(Train_Ma,Train_Lab,lambda1,lambda2,lambda3);
+[P,Q,Z,E,H,obj] = LLRSE(TrainData,TrainLab,lambda1,lambda2,lambda3);
 
-Test_Maa  = Q'*Test_Ma;
+Test_Maa  = Q'*TestData;
 Test_Maa = NormalizeFea(Test_Maa,0);
-Train_Maa = Q'*Train_Ma;
+Train_Maa = Q'*TrainData;
 Train_Maa = NormalizeFea(Train_Maa,0);
-[pred] = knnclassify(real(Test_Maa'),real(Train_Maa'),Train_Lab,1,'euclidean','nearest');
-acc_test     = sum(Test_Lab == pred)/length(Test_Lab)*100
+[pred] = knnclassify(real(Test_Maa'),real(Train_Maa'),TrainLab,1,'euclidean','nearest');
+acc_test     = sum(TestLab == pred)/length(TestLab)*100
 
